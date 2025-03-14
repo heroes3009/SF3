c0c67c927a5a2b68a784c4a41add05e1982a1f47
heroes3009 <heroes3009@yahoo.co.uk>
2025-03-14 16:23:52 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,3 +33,7 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie '+ name_on_order +'is ordered!', icon="✅")
 
+# New section to display smoothiefroot nutrition information
+import requests
+smoothiefroot_response = requests.get ("https: //my.smoothiefroot.com/api/fruit/watermelon"')
+st.text (smoothiefroot_response)
